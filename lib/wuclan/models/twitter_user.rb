49100a912cf85b5c829ea77b0d2d45ef2dcc51b0
Philip (flip) Kromer <flip@infochimps.org>
2009-06-24 08:36:39 -0500

    Brought rest of models across

--- lib/wuclan/models/twitter_user.rb
+++ lib/wuclan/models/twitter_user.rb
@@ -4,51 +4,19 @@ module Wuclan::Models
   # Mixin: common methods for each of the user representations / partitions
   #
   module TwitterUserCommon
-    # #
-    # # Datatype info, for exporting strings
-    # #
-    # MEMBERS_TYPES = {
-    #   :created_at,          :date,
-    #   :scraped_at,          :date,
-    #   :screen_name,         :str,
-    #   :protected,           :bool,
-    #   :followers_count,     :int,
-    #   :friends_count,       :int,
-    #   :statuses_count,      :int,
-    #   :favourites_count,    :int,
-    #   :name,                :str,
-    #   :url,                 :str,
-    #   :location,            :str,
-    #   :description,         :str,
-    #   :time_zone,           :str,
-    #   :utc_offset,          :int,
-    #   # :profile_background_color,       :str,
-    #   # :profile_text_color,             :str,
-    #   # :profile_link_color,             :str,
-    #   # :profile_sidebar_border_color,   :str,
-    #   # :profile_sidebar_fill_color,     :str,
-    #   # :profile_background_tile,        :bool,
-    #   # :profile_background_image_url,   :str,
-    #   # :profile_image_url,              :str,
-    # }
-    # def members_with_types
-    #   @members_with_types ||= MEMBERS_TYPES.slice(*members.map(&:to_sym))
-    # end
-
     #
     # Key on id
     #
     # For mutability (preserving different scraped_at observations)
     # needs to be 2 -- id and scraped_at
     #
-    def num_key_fields()  1  end
+    def num_key_fields()        1       end
     def numeric_id_fields()     [:id] ; end
-
     def decoded_name
-      @decoded_name        ||= (name        ? name.wukong_decode : '')
+      @decoded_name        ||= (name        ? name.wukong_decode        : '')
     end
     def decoded_location
-      @decoded_location    ||= (location    ? location.wukong_decode : '')
+      @decoded_location    ||= (location    ? location.wukong_decode    : '')
     end
     def decoded_description
       @decoded_description ||= (description ? description.wukong_decode : '')
@@ -73,7 +41,6 @@ module Wuclan::Models
     include TwitterUserCommon
     alias_method :tweets_count,    :statuses_count
     alias_method :favorites_count, :favourites_count
-
     #
     # Rate info
     #
@@ -81,7 +48,6 @@ module Wuclan::Models
     def followers_per_day()    followers_count.to_i / days_since_created  end
     def favorites_per_day()    favorites_count.to_i / days_since_created  end
     def tweets_per_day()       tweets_count.to_i    / days_since_created  end
-
   end
 
   #
@@ -155,9 +121,5 @@ module Wuclan::Models
     include ModelCommon
     include TwitterUserCommon
     def num_key_fields()  1  end
-    # def initialize id=nil, *args
-    #   super id, *args
-    #   self.id = "%010d" % id.to_i if id
-    # end
   end
 end
