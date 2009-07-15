81077cb0c86a4dc479a515461fb4e9fb14e0ca7d
Philip (flip) Kromer <flip@infochimps.org>
2009-07-15 09:54:02 -0500

    componentized scraper pagination methods

--- scrape_twitter_search.rb
+++ scrape_twitter_search.rb
@@ -10,11 +10,9 @@ require 'monkeyshines/scrape_request/raw_json_contents'
 require 'trollop'
 require File.dirname(__FILE__)+'/config/config_private'
 opts = Trollop::options do
-  opt :from,      "Flat file of request parameters", :type => String
-  opt :type,      "Class to instantiate from each line of the --from file, e.g. --type=ShortUrlHeadRequest", :type => String
-  opt :dumpfile_dir, "Filename base to store output. e.g. --dump_basename=/data/ripd", :type => String
-  opt :dumpfile_pattern, "Pattern for dump file output",
-    :default => ":revdom/:date/:revdom+:datetime-:pid.tsv"
+  opt :from,             "Flat file of request parameters", :type => String
+  opt :dumpfile_dir,     "Filename base to store output. e.g. --dump_basename=/data/ripd", :type => String
+  opt :dumpfile_pattern, "Pattern for dump file output", :default => ":revdom2/:revdom/:date/:revdom+:datetime-:pid.tsv"
 end
 
 class TwitterSearchRequest < Monkeyshines::ScrapeRequest
@@ -22,33 +20,32 @@ class TwitterSearchRequest < Monkeyshines::ScrapeRequest
   def items
     parsed_contents['results']
   end
+  def healthy?
+    items && items.is_a?(Array)
+  end
+
+  def num_items()
+    items ? items.length : 0
+  end
+  def id_span
+    [items.first['id'], items.last['id']] rescue nil
+  end
+  def time_span
+    [Time.parse(items.first['created_at']), Time.parse(items.last['created_at'])] rescue nil
+  end
+
 end
 
-class TwitterSearchScrapable < Struct.new(:query_term,
-    :first_item_id, :last_item_id,
-    :first_item_at, :last_item_at, :num_items
+class TwitterSearchScrapable < Struct.new(
+    :query_term
     )
   include Monkeyshines::Paginated #WithLimit
   self.items_per_page = 100
-  self.max_pages      = 2
-  attr_accessor :max_id
+  self.max_pages      = 15
 
   def initialize *args
     super *args
-    self.num_items ||= 0
-  end
-
-  def acknowledge response
-    return unless response && response.items
-    self.num_items += response.items.length
-    if (first_item = response.items.first)
-      self.first_item_at ||= Time.parse(first_item['created_at'])
-      self.first_item_id ||= first_item['id']
-    end
-    if (last_item = response.items.last)
-      self.last_item_at    = Time.parse(last_item['created_at'])
-      self.last_item_id    = last_item['id']
-    end
+    begin_pagination!
   end
 
   # "since_id":0,
@@ -57,9 +54,8 @@ class TwitterSearchScrapable < Struct.new(:query_term,
   # "next_page":"?page=2&max_id=1480307926&q=%40twitterapi",
   def make_request page, pageinfo
     url_str = "http://search.twitter.com/search.json?q=#{query_term}&rpp=#{items_per_page}"
-    url_str << "&max_id=#{max_id}"   if max_id
-    url_str << "&page=#{page}"       if page.to_i > 0
-    # url_str << "&since_id=#{min_id}" if min_id
+    url_str << "&max_id=#{unscraped_id_span.max-1}" if unscraped_id_span.max
+    #url_str << "&page=#{page}"                      if page.to_i > 0
     TwitterSearchRequest.new url_str
   end
 end
@@ -89,12 +85,15 @@ scraper = Monkeyshines::ScrapeEngine::HttpScraper.new Monkeyshines::CONFIG[:twit
 
 #
 search_term_scrapables.each do |search_term_scrapable|
+  next if (search_term_scrapable.query_term =~ /^#/) || (search_term_scrapable.query_term.blank?)
+  # search_term_scrapable.prev_id_span.max = 2530000000
   search_term_scrapable.each_page do |req|
     response = scraper.get(req)
     #   if response
     #     # save the response
     #     store.save response
     #   end
+    response
   end
 
   # # store completed request with updated min_id
