252f4007969769cdd2434fb77cb49457055b6c50
Philip (flip) Kromer <flip@infochimps.org>
2009-07-28 12:21:46 -0500

    Added Bogus class. don't know yet if this goes in main line...

--- lib/wuclan/domains/twitter/scrape/old_skool_request_classes.rb
+++ lib/wuclan/domains/twitter/scrape/old_skool_request_classes.rb
@@ -1,18 +1,26 @@
-
 module Wuclan::Domains::Twitter::Scrape
-#
-# Older versions of wuclan had a slightly different request naming scheme, and
-# had additional specialized fields. This module adapts old to new; you're only
-# likely to need this if you're me, @mrflip.
-#
-module OldSkoolRequest
-  def initialize(priority, twitter_user_id, page, screen_name, *args) super(*args) ;
-    self.twitter_user_id = twitter_user_id
+  #
+  # Older versions of wuclan had a slightly different request naming scheme, and
+  # had additional specialized fields. This module adapts old to new; you're only
+  # likely to need this if you're me, @mrflip.
+  #
+  module OldSkoolRequest
+    def initialize(priority, twitter_user_id, page, screen_name, *args) super(*args) ;
+      self.twitter_user_id = twitter_user_id
+    end
   end
-end
-class Followers < TwitterFollowersRequest ; include OldSkoolRequest ; end
-class Friends   < TwitterFriendsRequest   ; include OldSkoolRequest ; end
-class Favorites < TwitterFavoritesRequest   ; include OldSkoolRequest ; end
-class UserTimeline < TwitterUserTimelineRequest   ; include OldSkoolRequest ; end
-
+  class Followers < TwitterFollowersRequest       ; include OldSkoolRequest ; end
+  class Friends   < TwitterFriendsRequest         ; include OldSkoolRequest ; end
+  class Favorites < TwitterFavoritesRequest       ; include OldSkoolRequest ; end
+  class UserTimeline < TwitterUserTimelineRequest ; include OldSkoolRequest ; end
+  class Bogus < Base ;
+    include OldSkoolRequest
+    # attr_accessor :contents
+    # def initialize *contents
+    #   self.contents = contents
+    # end
+    def parse
+      yield self
+    end
+  end
 end
