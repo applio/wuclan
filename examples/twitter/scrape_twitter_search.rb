d48d39ff46a76936908664f8bec3537bedb79051
Philip (flip) Kromer <flip@infochimps.org>
2009-08-21 11:18:27 -0500

    New twitter search

--- examples/twitter/scrape_twitter_search.rb
+++ examples/twitter/scrape_twitter_search.rb
@@ -1,58 +1,35 @@
 #!/usr/bin/env ruby
-$: << ENV['WUKONG_PATH']
-require File.dirname(__FILE__)+'/config/config_private'
 require 'rubygems'
-require 'trollop'
-require 'wukong'
 require 'monkeyshines'
-require 'wuclan/domains/twitter/scrape' ; include Wuclan
+require 'monkeyshines/recursive_runner'
+WORK_DIR = Subdir[__FILE__,'work'].expand_path
+puts WORK_DIR
 
-require 'monkeyshines/fetcher/http_fetcher'
-require 'monkeyshines/utils/filename_pattern'
 #
-# Command line options
+# Set up scrape
 #
-opts = Trollop::options do
-  opt :handle,              "Handle to uniquely identify this scrape",          :default => 'com.twitter.search'
-  opt :items_per_job,       "Desired item count per job",                       :default => 1000
-  opt :min_resched_delay,   "Don't run jobs more often than this (in seconds)", :default => 20*1
-  opt :job_db,              "Tokyo tyrant db host",                             :default => ':1978', :type => String
-  opt :log,                 "Log file name; leave blank to use STDERR",         :type => String
-  # output storage
-  opt :chunk_time,     "Frequency to rotate chunk files (in seconds)", :type => Integer, :default => 60*60*4
-  opt :dest_dir,       "Filename base to store output. e.g. --dump_basename=/data/ripd", :type => String
-  opt :dest_pattern,   "Pattern for dump file output",                         :default => Monkeyshines::Utils::FilenamePattern::DEFAULT_PATTERN_STR
-end
-Trollop::die :dest_dir unless opts[:dest_dir]
-Monkeyshines.logger = Logger.new(opts[:log], 'daily') if opts[:log]
+Monkeyshines.load_cmdline_options!
+Monkeyshines.load_global_options!(Monkeyshines::CONFIG[:handle])
+require 'wuclan/lastfm' ; include Wuclan::Lastfm::Scrape
 
-# Queue of request scrape_jobs, with reschedule requests
-beanstalk_tube  = opts[:handle].gsub(/\w+/,'_')
-request_queue   = Monkeyshines::RequestStream::BeanstalkQueue.new(nil, Twitter::Scrape::TwitterSearchJob, opts[:items_per_job], opts.slice(:min_resched_delay))
-# Scrape Store for completed requests
-dest_pattern    = Monkeyshines::Utils::FilenamePattern.new(opts[:dest_pattern], opts.slice(:handle, :dest_dir))
-dest            = Monkeyshines::Store::ChunkedFlatFileStore.new dest_pattern, opts[:chunk_time].to_i
-# Scrape requests by HTTP
-fetcher         = Monkeyshines::Fetcher::HttpFetcher.new Monkeyshines::CONFIG[:twitter]
-# Log every 60 seconds
-periodic_log    = Monkeyshines::Monitor::PeriodicLogger.new(:time => 60)
-# Persist scrape_job jobs in distributed DB
-job_store       = Monkeyshines::Store::TyrantTdbKeyStore.new(opts[:job_db])
+#
+# Create runner
+#
+scraper = Monkeyshines::RecursiveRunner.new({
+    :log     => { :iters => 100, :dest => Monkeyshines::CONFIG[:handle] },
+    :source  => { :type  => Monkeyshines::RequestStream::KlassHashRequestStream,
+      :store => { :type => Monkeyshines::RequestStream::EdamameQueue,
+        :store => { :uri =>            ':11212',  :type => 'TyrantStore',    },
+        :queue => { :uris => ['localhost:11210'], :type => 'BeanstalkQueue', } }, },
+    :dest    => { :type  => :conditional_store,
+      :cache => { :uri =>              ':11222', },
+      :store => { :rootdir => WORK_DIR },},
+    # :fetcher => { :type => :fake_fetcher },
+    :force_fetch => false,
+    :sleep_time  => 0.2,
+  })
 
-request_queue.each do |scrape_job|
-  # Run through all pages for this search term
-  scrape_job.each_request do |req|
-    # Fetch request
-    response = fetcher.get(req)
-    # save it if successful
-    dest.save response if response
-    # log progress
-    periodic_log.periodically{ ["%7d"%response.num_items, response.url] }
-    # return it to the scrape_job for bookkeeping
-    response
-  end
-  # Persist the updated job to the scrape_jobs db, so that we can restart queue easily
-  job_store.save "#{scrape_job.class}-#{scrape_job.query_term}", scrape_job.to_hash.compact
-  # sleep 0.5
+# Execute the scrape
+loop do
+  scraper.run
 end
-request_queue.close
